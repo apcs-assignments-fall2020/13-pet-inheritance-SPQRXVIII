ed7050f317b484e9ea215dc7796d4567b819a82e
SPQRXVIII <Aftersilver001@gmail.com>
2020-11-03 16:16:43 +0000

    finished

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,45 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String breed;
 
+    public Dog(){
+        setAge(0);
+        setName("Jack");
+        setBreed("none");
+    }
 
     // Constructors
-    
+    public Dog(String name, int age, String breed) {
+        setAge(age);
+        setName(name);
+        setBreed(breed);
+       }
 
     // makeNoise() method
-
+       public void makeNoise() {
+        System.out.println("Woof!");
+    }
 
     // toString method
+    public String toString() {
+        String str = "Name: " + getName() + ", Age: " + getAge()+", Breed: "+getBreed();
+        return str;
+    }
 
 
     // Getter
+    public String getBreed() {
+        return this.breed;
+    }
+
 
 
     // Setter
+    public void setBreed(String breed) {
+        // Don't allow blank names
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
+
 }
